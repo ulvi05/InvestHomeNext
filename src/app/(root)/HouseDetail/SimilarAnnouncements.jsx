93a9297358523ad83f742ab7cd0ef130a351e487
mirhuseyntalibzade <mirhuseynet-ab205@code.edu.az>
2025-07-26 14:09:02 +0400

    fixed house detail responsiveness

--- src/app/(root)/HouseDetail/SimilarAnnouncements.jsx
+++ src/app/(root)/HouseDetail/SimilarAnnouncements.jsx
@@ -4,21 +4,21 @@ import HouseCard from '@/components/ui/HouseCard';
 const SimilarAnnouncements = () => {
   return (
     <>
-    <div className='max-w-full w-auto px-[80px] flex flex-col items-start justify-center'>
-      <h1 className="text-[#111] text-[24px] sm:text-[28px] font-medium">
-        Oxşar elanlar
-      </h1>
+      <div className='max-w-full w-auto px-[80px] flex flex-col items-start justify-center'>
+        <h1 className="text-[#111] text-[24px] sm:text-[28px] font-medium">
+          Oxşar elanlar
+        </h1>
 
 
-      <div className='w-full h-auto flex flex-row items-center justify-between gap-y-10 flex-wrap mt-[62px] mb-[544px]'>
-        {houseData.map((house) => (
-          <HouseCard
-            key={house.id}
-            house={house}
-          />
-        ))}
+        <div className='w-full grid grid-cols-4 max-[1025px]:grid-cols-3 max-[769px]:grid-cols-2 max-[426px]:grid-cols-1 gap-[24px]'>
+          {houseData.map((house) => (
+            <HouseCard
+              key={house.id}
+              house={house}
+            />
+          ))}
+        </div>
       </div>
-    </div>
 
 
     </>
