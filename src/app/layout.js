8234a6f4d672eae0671711e02567fe2938f70f76
mirhuseyntalibzade <mirhuseynet-ab205@code.edu.az>
2025-08-04 21:20:03 +0400

    implemented pwa

--- src/app/layout.js
+++ src/app/layout.js
@@ -26,6 +26,10 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="theme-color" content="#000000" />
+        <link rel="apple-touch-icon" href="/icons/icon-192x192.png" />
+        <meta name="apple-mobile-web-app-title" content="Invest Home" />
         <script
           src="https://kit.fontawesome.com/066942706e.js"
           crossOrigin="anonymous"
